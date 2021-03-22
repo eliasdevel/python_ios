8ac6b9447d2c323932875dd0046fef963051bfcb
Elias Müller <elias.muller1@gmail.com>
2021-03-22 10:24:15 -0300

    ajust

--- io_service.py
+++ io_service.py
@@ -2,7 +2,7 @@
 from pymongo import MongoClient
 import datetime
 import time
-
+import re
 
 client = MongoClient('localhost', 27017)
 
@@ -12,13 +12,28 @@ collection = db.sensors
 
 
 
-cursor = collection.find(tailable=True,await_data=True)
-while cursor.alive:
-    try:
-        doc = cursor.next()
-        print doc
-    except StopIteration:
-        time.sleep(1)
+def get_cursor(collection, topic_re, last_id=-1, await_data=True):
+    options = { 'tailable': True }
+    spec = { 
+        'ts': { '$gt': last_id }, # only new messages
+        'k': topic_re }
+    if await_data:
+        options['await_data'] = True
+    cur = collection.find(spec, **options)
+    cur = cur.hint([('$natural', 1)]) # ensure we don't use any indexes
+    return cur
+    
+
+while True:
+    cur = get_cursor(
+        db.sensors, 
+        re.compile('^foo'), 
+        await_data=True)
+    for msg in cur:
+        last_id = msg['ts']
+        print(msg)
+    time.sleep(0.1)
+
 
 
 
