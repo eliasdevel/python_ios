9962ecdc96d7dbf64cfa9eae8adc495a32e843f1
Elias Müller <elias.muller1@gmail.com>
2021-05-18 20:06:35 -0300

    ajust

--- io_service.py
+++ io_service.py
@@ -19,12 +19,12 @@ for x in collection.find():
 
 def setup():
     gpio.setmode(gpio.BOARD)
-    gpio.setup(1, gpio.OUT)
-    gpio.setup(2, gpio.OUT)
-    gpio.setup(3, gpio.OUT)
-    gpio.setup(4, gpio.OUT)
     gpio.setup(5, gpio.OUT)
     gpio.setup(6, gpio.OUT)
+    gpio.setup(7, gpio.OUT)
+    gpio.setup(8, gpio.OUT)
+    gpio.setup(9, gpio.OUT)
+    gpio.setup(10, gpio.OUT)
 
 
 setup()
