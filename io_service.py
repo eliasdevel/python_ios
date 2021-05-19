7994b60476a054ac4d65f8fc3f92dc924281f581
Elias Müller <elias.muller1@gmail.com>
2021-05-18 21:01:42 -0300

    ajust

--- io_service.py
+++ io_service.py
@@ -37,7 +37,7 @@ while True:
             values[item['_id']] = item['valor_atual'];
             
             
-            if(item['valor_ideal'] > item['valor_acionamento'] ):
+            if(float(item['valor_ideal']) > float(item['valor_acionamento']) ):
                 print('\n\naquecer\n\n')
                 if(item['valor_atual'] <= item['valor_acionamento']):
                     print('acionando o sensor %d', item['aciona_saida'])
@@ -46,6 +46,7 @@ while True:
                     if(item['valor_atual'] >= item['valor_ideal']):
                         gpio.output(int(item['aciona_saida']), gpio.LOW)
                         print('desligando sensor %d', item['aciona_saida'])
+
              
             if(item['valor_ideal'] < item['valor_acionamento']):
                 print('\n\nresfriar\n\n')
